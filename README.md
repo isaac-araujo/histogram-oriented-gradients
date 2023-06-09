b3b7870a3351472a1d3dfedda13c8f8db775f540
Isaac <zac.araujo2001@gmail.com>
2023-06-08 23:15:24 -0300

    initial

--- README.md
+++ README.md
@@ -2,18 +2,37 @@
 
 Individual - Valor 1.0 ponto.
 Formato da entrega: repositorio git.
-Data da entrega: 
+Data da entrega:
 08/06/2023 - 23:59
 Para este trabalho o aluno pode escolher uma das duas opções de entrega que deseja fazer.
 Implementação do algoritmo para descritor HoG sem utilização de bibliotecas e métodos prontos.
 Implementação de um classificador utilizando a biblioteca pronta para HoG.
 
-
 Especificação do HoG (SEM BIBLIOTECA)
 
 IImplementação do algoritmo para descritor HoG (não utilizar bibliotecas prontas para o algoritmo - magnitude, orientação e histogramas precisam ser feito manualmente)
 Seguir o algoritmo proposto para gerar o descritor para as imagens.
-Salve os valores finais do descritor HoG. 
+Salve os valores finais do descritor HoG.
 Salve 5 imagens com as orientações nos histogramas das células mapeadas no começo do algoritmo.
 Todo o código e as imagens precisam estar versionadas.
-Passo a passo para construção do algoritmo AQUI no slide 41 
+Passo a passo para construção do algoritmo AQUI no slide 41
+
+---
+
+
+
+1. Redimensione a imagem (altura e largura) para um múltiplo de 8.
+2. Achar a magnitude e orientação de cada pixel
+3. 1. Magnitude => Dx e Dy com os filtros 1x3 e 3x1
+   2. Orientação => Função arctan (Dx/Dy)
+4. Dividir a imagem em células de 8x8 pixels
+5. A cada célula pega a magnitude e orientação (128 valores por célula)
+6. Para cada célula 8x8, calcula-se um histograma de orientações.
+7. Padrão: 9 faixas (passos de 20 graus).
+8. Distribua o valor da magnitude de cada pixel da célula em até 2 faixas do histograma (exemplo slide 34)
+9. Agrupe 4 células em um bloco 16x16 (2 células de largura e 2 de altura)
+10. Bloco => 4 histogramas, com 9 faixas cada = 36 valores por bloco
+11. Normalize os valores de cada bloco utilizando L2
+12. Calcular a raiz da soma de todos os valores ao quadrado, e então dividir todos os valores por este resultado
+13. Descrito Hog é a concatenação dos valores normalizados de cada bloco.
+14. Para visualizá-lo utiliza-se as orientações nos histogramas das células, feito no passo 3.
---
+++ main.py
@@ -0,0 +1,89 @@
+import os
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def resize_image(image):
+    # Resize
+    height, width = image.shape[:2]
+    new_height = height - (height % 8)
+    new_width = width - (width % 8)
+    return cv2.resize(image, (new_width, new_height))
+
+
+def compute_gradients(image):
+    dx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
+    dy = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
+    # Magnitude
+    magnitude = cv2.addWeighted(dx, 0.5, dy, 0.5, 0)
+    # cv2.imshow("magnitude", magnitude)
+    # Orientation
+    orientation = np.arctan2(dy, dx)
+    # cv2.imshow("orientation", orientation)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return magnitude, orientation
+
+
+def compute_histogram(orientation, magnitude, num_bins=9):
+    hist = np.zeros(num_bins)
+    bin_width = 360 / num_bins
+    orientation = orientation % 360
+    for i in range(num_bins):
+        hist[i] = np.sum(
+            magnitude[
+                np.where((orientation >= i * bin_width) & (orientation < (i + 1) * bin_width))
+            ]
+        )
+    return hist
+
+
+def normalize_cell(cell):
+    norm = np.linalg.norm(cell)
+    if norm != 0:
+        cell /= norm
+    return cell
+
+
+def compute_hog(image, cell_size=(8, 8), num_bins=9):
+    image = resize_image(image)
+    magnitude, orientation = compute_gradients(image)
+
+    height, width = magnitude.shape[:2]
+    # cells_per_block = (block_size[0] // cell_size[0], block_size[1] // cell_size[1])
+    num_cels = (height // cell_size[0], width // cell_size[1])
+    hog_descriptor = []
+    hog_image = np.zeros((height, width), dtype=float)
+    for i in range(num_cels[0]):
+        for j in range(num_cels[1]):
+            cell_mag = magnitude[
+                i * cell_size[0] : (i + 1) * cell_size[0],
+                j * cell_size[1] : (j + 1) * cell_size[1]
+            ]
+            cell_ori = orientation[
+                i * cell_size[0] : (i + 1) * cell_size[0],
+                j * cell_size[1] : (j + 1) * cell_size[1],
+            ]
+            cell_histogram = compute_histogram(cell_ori, cell_mag, num_bins)
+            # normalized_cell = normalize_cell(cell_histogram)
+            # hog_descriptor.extend(normalized_cell)
+            hog_image[
+                i * cell_size[0] : (i + 1) * cell_size[0], j * cell_size[1] : (j + 1) * cell_size[1]
+            ] = cell_histogram[0]
+    hog_descriptor = np.array(hog_descriptor)
+    return hog_image
+
+
+for name_img in os.listdir("images"):
+    image = cv2.imread(f"images/{name_img}", cv2.IMREAD_GRAYSCALE)
+
+    hog_image = compute_hog(image)
+
+    # cv2.imwrite(f"images_hog/{name_img}", hog_image)
+    plt.imshow(hog_image, cmap="gray")
+    plt.axis("off")
+    plt.savefig(f"images_hog/{name_img}")
+
+    # plt.show()
